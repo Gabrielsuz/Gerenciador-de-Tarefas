0a546de77b195fbba4e7e59bff9527e5e3bea286
Gabrielsuz <mestredejoga@gmail.com>
2026-05-25 16:46:16 -0300

    Adicionando sistema de tempo.

--- script.js
+++ script.js
@@ -6,20 +6,31 @@ $(function() {
     $("#confirmar").click(function () { 
         let texto = $("#texto").val();
         let titulo = $("#titulo").val();
+        let tempo = parseInt($("#temp").val() * 1000);
         let getNum = parseInt(localStorage.getItem('numeroTarefa'))
         getNum = getNum + 1
 
         localStorage.setItem('numeroTarefa',getNum)
-        document.set_cookie()
 
         if (texto == "") {
             alert("Insira uma descrição para a tarefa.")
         } else {
             if (titulo == "") {
-                $("#lista").append('<div id="pronto" class="tarefa"><div class="info"><button id="close">X</button> <button id="change"><i class="fa-solid fa-chart-simple"></i> </button> <span id="status">Concluída</span></div>  <h1>TAREFA #0'+getNum+'</h1><p>'+texto+'</p> </div>');
+                $("#lista").append('<div id="pronto" data-id="'+ getNum +'" class="tarefa" ><div class="info"><button id="close">X</button> <button id="change"><i class="fa-solid fa-chart-simple"></i> </button> <span id="status">Concluída</span></div>  <h1>TAREFA #0'+getNum+'</h1><p>'+texto+'</p> </div>');
             } else {
-                $("#lista").append('<div id="pronto" class="tarefa"><div class="info"><button id="close">X</button> <button id="change"><i class="fa-solid fa-chart-simple"></i> </button> <span id="status">Concluída</span></div>  <h1>'+titulo+'</h1><p>'+texto+'</p> </div>');
+                $("#lista").append('<div id="pronto" data-id="'+ getNum +'" class="tarefa"><div class="info"><button id="close">X</button> <button id="change"><i class="fa-solid fa-chart-simple"></i> </button> <span id="status">Concluída</span></div>  <h1>'+titulo+'</h1><p>'+texto+'</p> </div>');
             }
+        }
+        
+        if (tempo != null && tempo > 0 && typeof tempo === "number") {
+            setTimeout(() => {
+                let tarefa = $('.tarefa[data-id="' + getNum + '"]');
+                console.log("Excluido")
+                tarefa.remove();
+            }, tempo);
+        console.log(tempo)
+        } else {
+            alert("O tempo: "+ $("#temp").val() +" é invalido.")
         }
         
     });
